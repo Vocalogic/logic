6a233e0d0aa4aa3e5ed674410ff02c43d35ee76e
vlchris <chris@vocalogic.com>
2023-01-17 10:22:03 -0500

    HOTFIX: Fix footer for showing active version

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,12 +2,11 @@
 <footer class="page-footer px-xl-4 px-sm-2 px-0 py-3">
     <div class="container-fluid d-flex flex-wrap justify-content-between align-items-center">
         <p class="col-md-4 mb-0 text-muted">© {{date("Y")}} <a href="{{setting('brand.url')}}" target="_blank"
-                                                               title="{{setting('brand.name')}}">{{setting('brand.name')}}</a> |
-            @if(isAdmin())
-                <a href="/admin/versions">Logic v{{setting('version')}}</a>
-            @else
-                Logic v{{setting('version')}}
-            @endif
+                                                               title="{{setting('brand.name')}}">{{setting('brand.name')}}</a>
+            |
+
+            Logic v{{currentVersion()->version}}
+
 
         </p>
         <a href="#" class="col-md-4 d-flex align-items-center justify-content-center my-3 my-lg-0 me-lg-auto">
