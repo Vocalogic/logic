1bdb6617328c8febda20501b840ef3a8e89af28a
vlchris <chris@vocalogic.com>
2023-01-22 14:40:39 -0500

    fixed: typo in class for submit wait corrected

--- resources/views/admin/bill_items/create_modal.blade.php
+++ resources/views/admin/bill_items/create_modal.blade.php
@@ -6,35 +6,33 @@
 <form method="POST" action="/admin/category/{{$cat->id}}/items" class="createForm">
     @method('POST')
     @csrf
-<div class="card border-primary">
-    <div class="card-body">
-    <div class="row">
-        <div class="row  mt-2">
-            <x-form-input name="code" label="{{ucfirst($type)}} Code/SKU" icon="database">
-                Enter a code/sku to define this item.
-            </x-form-input>
+    <div class="card border-primary">
+        <div class="card-body">
+            <div class="row">
+                <div class="row  mt-2">
+                    <x-form-input name="code" label="{{ucfirst($type)}} Code/SKU" icon="database">
+                        Enter a code/sku to define this item.
+                    </x-form-input>
 
-            <x-form-input name="name" label="{{ucfirst($type)}} Name" icon="bars">
-                Enter name to be used on invoice/quote/shop
-            </x-form-input>
+                    <x-form-input name="name" label="{{ucfirst($type)}} Name" icon="bars">
+                        Enter name to be used on invoice/quote/shop
+                    </x-form-input>
 
-            <x-form-input name="price" label="Selling Price" icon="dollar">
-                Enter the selling price for this item.
-            </x-form-input>
-        </div>
+                    <x-form-input name="price" label="Selling Price" icon="dollar">
+                        Enter the selling price for this item.
+                    </x-form-input>
+                </div>
 
-        <div class="row mt-3">
-            <div class="offset-4 col-8">
-                <input type="submit" class="w-100 btn btn-{{bm()}}primary wait"
-                       data-anchor="createForm"
-                       data-message="Creating Item.."
-                       value="Save">
+                <div class="row mt-3">
+                    <div class="offset-4 col-8">
+                        <input type="submit" class="w-100 btn btn-{{bm()}}primary wait"
+                               data-anchor=".createForm"
+                               data-message="Creating Item.."
+                               value="Save">
+                    </div>
+                </div>
             </div>
+
         </div>
     </div>
-
-    </div>
-</div>
-
-
 </form>
