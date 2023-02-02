14652ec664be224a249a2a13e4f4f6ee8181bd72
vlchris <chris@vocalogic.com>
2023-02-02 13:41:38 -0500

    Moved service billing area to billing and removed tab in services

--- resources/views/admin/accounts/billing/fields.blade.php
+++ resources/views/admin/accounts/billing/fields.blade.php
@@ -9,7 +9,7 @@
 
         <div class="card">
             <div class="card-body">
-                <h6 class="card-title">Billing Settings</h6>
+                <h6 class="card-title">Invoice Settings</h6>
                 <p>
                     If you would like for invoices and other billing emails to go to a specific email
                     address, you can add it below, otherwise it will use the primary user on the account.
@@ -33,7 +33,7 @@
                         </div>
                         <div class="col-lg-12 mt-2">
                             <button type="submit" class="btn btn-primary ladda pull-right" data-style="zoom-out">
-                                <i class="fa fa-save"></i> Save Billing Settings
+                                <i class="fa fa-save"></i> Save Invoice Settings
                             </button>
                         </div>
                     </div>
@@ -41,6 +41,74 @@
             </div>
 
         </div>
+
+
+        <div class="card mt-4">
+            <div class="card-body">
+                <p class="card-title">Service Billing Properties</p>
+                <form method="POST" action="/admin/accounts/{{$account->id}}" class="properties">
+                    @method('PUT')
+                    @csrf
+                    <div class="row">
+                        <div class="col-lg-12 col-md-12">
+                            <div class="form-floating">
+                                <input type="date" class="form-control" name="next_bill"
+                                       value="{{$account->next_bill?->format("Y-m-d")}}">
+                                <label>Next Bill Date</label>
+                                <span class="helper-text">Update the next service invoice date.</span>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-12 col-md-12 mt-2">
+                            <div class="form-floating">
+                                <input type="text" class="form-control" name="bills_on" value="{{$account->bills_on}}">
+                                <label>Bill Day of Month</label>
+                                <span class="helper-text">This account always bills on which day?</span>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-12 col-md-12 mt-2">
+                            <div class="form-floating">
+                                <input type="text" class="form-control" name="net_terms"
+                                       value="{{$account->net_terms}}">
+                                <label>NET Terms</label>
+                                <span class="helper-text">Days given to pay bill before late?</span>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-12 col-md-12 mt-2">
+                            <div class="form-floating">
+                                {!! Form::select('payment_method', \App\Enums\Core\PaymentMethod::selectable(), $account->payment_method?->value, ['class' => 'form-control']) !!}
+                                <label>Payment Method</label>
+                                <span class="helper-text">Default Payment Method for Monthly Invoices</span>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-12 col-md-12 mt-2">
+                            <div class="form-floating">
+                                {!! Form::select('auto_bill', [0 => 'No', 1 => 'Yes'], $account->auto_bill, ['class' => 'form-control']) !!}
+                                <label>Auto-Bill on Due Date?</label>
+                                <span class="helper-text">If Yes, Logic will attempt to bill customers payment method automatically on due date.</span>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-12 col-md-12 mt-3">
+                            <button type="submit" class="btn btn-primary ladda" data-style="zoom-out">
+                                <i class="fa fa-save"></i> Update Service Billing
+                            </button>
+
+                        </div>
+
+                    </div>
+                </form>
+            </div>
+        </div>
+
+
+
     </div>
+
+
+
 
 </div>
--- resources/views/admin/accounts/services/bill_date.blade.php
+++ resources/views/admin/accounts/services/bill_date.blade.php
@@ -3,73 +3,10 @@
         <a class="nav-link active" data-bs-toggle="tab" href="#profit" role="tab">Profit ({{$account->analysis->margin}}%)</a>
     </li>
     <li class="nav-item">
-        <a class="nav-link" data-bs-toggle="tab" href="#settings" role="tab">Settings</a>
-    </li>
-    <li class="nav-item">
         <a class="nav-link" data-bs-toggle="tab" href="#serviceAction" role="tab">Actions</a>
     </li>
 </ul>
 <div class="tab-content">
-    <div class="tab-pane fade" id="settings" role="tabpanel">
-
-        <div class="card">
-            <div class="card-body">
-                <p class="card-title">Service Billing Properties</p>
-                <form method="POST" action="/admin/accounts/{{$account->id}}" class="properties">
-                    @method('PUT')
-                    @csrf
-                    <div class="row">
-                        <div class="col-lg-12 col-md-12">
-                            <div class="form-floating">
-                                <input type="date" class="form-control" name="next_bill"
-                                       value="{{$account->next_bill?->format("Y-m-d")}}">
-                                <label>Next Bill Date</label>
-                                <span class="helper-text">Update the next service invoice date.</span>
-                            </div>
-                        </div>
-
-                        <div class="col-lg-12 col-md-12 mt-2">
-                            <div class="form-floating">
-                                <input type="text" class="form-control" name="bills_on" value="{{$account->bills_on}}">
-                                <label>Bill Day of Month</label>
-                                <span class="helper-text">This account always bills on which day?</span>
-                            </div>
-                        </div>
-
-                        <div class="col-lg-12 col-md-12 mt-2">
-                            <div class="form-floating">
-                                <input type="text" class="form-control" name="net_terms"
-                                       value="{{$account->net_terms}}">
-                                <label>NET Terms</label>
-                                <span class="helper-text">Days given to pay bill before late?</span>
-                            </div>
-                        </div>
-
-                        <div class="col-lg-12 col-md-12 mt-2">
-                            <div class="form-floating">
-                                {!! Form::select('payment_method', \App\Enums\Core\PaymentMethod::selectable(), $account->payment_method?->value, ['class' => 'form-control']) !!}
-                                <label>Payment Method</label>
-                                <span class="helper-text">Default Payment Method for Monthly Invoices</span>
-                            </div>
-                        </div>
-
-                        <div class="col-lg-12 col-md-12 mt-2">
-                            <div class="form-floating">
-                                {!! Form::select('auto_bill', [0 => 'No', 1 => 'Yes'], $account->auto_bill, ['class' => 'form-control']) !!}
-                                <label>Auto-Bill on Due Date?</label>
-                                <span class="helper-text">If Yes, Logic will attempt to bill customers payment method automatically on due date.</span>
-                            </div>
-                        </div>
-
-                        <div class="col-lg-12 col-md-12 mt-3">
-                            <input type="submit" name="save" value="Save" class="btn btn-primary w-100 wait" data-anchor=".properties">
-                        </div>
-
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
     <div class="tab-pane  active" id="profit" role="tabpanel">
         @include('admin.accounts.services.profit')
     </div>
