2889c4ee4ec3aa1ec54dde72fff0a3f9699272f4
vlchris <chris@vocalogic.com>
2023-02-07 07:49:50 -0500

    added: relationships in applog

--- app/Models/AppLog.php
+++ app/Models/AppLog.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 /**
  * @property integer     $user_id
@@ -19,4 +20,33 @@ class AppLog extends Model
     use HasFactory;
 
     protected $guarded = ['id'];
+
+    /**
+     * A log entry can be attached to a user.
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * A log entry can be attached to an account/
+     * @return BelongsTo
+     */
+    public function account(): BelongsTo
+    {
+        return $this->belongsTo(Account::class);
+    }
+
+    /**
+     * Auto convert into a json object IF the data found
+     * is in fact a json object. Otherwise just return the text.
+     * @return mixed
+     */
+    public function getDetailFormatted(): mixed
+    {
+        if (json_decode($this->detail)) return json_decode($this->detail);
+        return $this->detail;
+    }
 }
