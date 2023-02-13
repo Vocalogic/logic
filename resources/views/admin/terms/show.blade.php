b15226397eba0a6cf9da08992eb31fe39ba37440
logan <lskidmore@netdepot.com>
2023-02-13 17:07:05 -0500

    reformatting tos show blade for create

--- resources/views/admin/terms/show.blade.php
+++ resources/views/admin/terms/show.blade.php
@@ -1,11 +1,12 @@
-@extends('layouts.admin', ['title' => $term->name])
+@extends('layouts.admin', ['title' => $term->name ?: "Create Service Terms"])
 
 
 @section('pre')
     <div class="row align-items-center">
         <div class="col-auto">
-            <h1 class="fs-5 color-900 mt-1 mb-0">{{$term->name}}</h1>
-            <small class="text-muted">Manage your Terms of Service for different types of accounts</small>
+            <h1 class="fs-5 color-900 mt-1 mb-0">{{$term->name?: "Create Service Terms"}}</h1>
+            <small class="text-muted">{{$term->name?"Manage your": "Create new"}} Terms of Service for
+                different types of accounts</small>
         </div>
 
     </div> <!-- .row end -->
@@ -15,9 +16,9 @@
 @section('content')
     <div class="row">
         <div class="col-lg-12">
-            <form method="post" action="/admin/terms/{{$term->id}}">
+            <form method="post" action={{$term->id ? "/admin/terms/$term->id" : "/admin/terms"}}>
                 @csrf
-                @method('PUT')
+                @method($term->id ? "PUT" : "POST")
                 <div class="row">
                     <div class="col-lg-12">
                         <textarea name="body" class="tinymce">{!! $term->body !!}</textarea>
@@ -25,7 +26,7 @@
                     <div class="row mt-3">
                         <div class="col-lg-12">
                             <button type="submit" class="btn btn-primary pull-right ladda" data-style="zoom-out">
-                                <i class="fa fa-save"></i> Save Service Terms
+                                <i class="fa fa-save"></i> {{$term->name?"Save": "Create"}} Service Terms
                             </button>
                         </div>
                     </div>
