ae2a099ff588dc01272139df8db7e723810f78b2
logan <lskidmore@netdepot.com>
2023-02-14 13:24:28 -0500

    added name text input on edit/create view

--- resources/views/admin/terms/show.blade.php
+++ resources/views/admin/terms/show.blade.php
@@ -1,11 +1,11 @@
-@extends('layouts.admin', ['title' => $term->name ?: "Create Service Terms"])
+@extends('layouts.admin', ['title' => $term->id ? "Edit: ".$term->name : "Create Service Terms"])
 
 
 @section('pre')
     <div class="row align-items-center">
         <div class="col-auto">
-            <h1 class="fs-5 color-900 mt-1 mb-0">{{$term->name?: "Create Service Terms"}}</h1>
-            <small class="text-muted">{{$term->name?"Manage your": "Create new"}} Terms of Service for
+            <h1 class="fs-5 color-900 mt-1 mb-0">{{$term->id? "Edit: ".$term->name : "Create Service Terms"}}</h1>
+            <small class="text-muted">{{$term->id?"Manage your": "Create new"}} Terms of Service for
                 different types of accounts</small>
         </div>
 
@@ -19,6 +19,14 @@
             <form method="post" action={{$term->id ? "/admin/terms/$term->id" : "/admin/terms"}}>
                 @csrf
                 @method($term->id ? "PUT" : "POST")
+                <div class="row mb-2">
+                    <div class="col-lg-12">
+                        <div class="form-floating">
+                            <input type="text" name="name" class="form-control" value="{{$term->name}}">
+                            <label>Name</label>
+                        </div>
+                    </div>
+                </div>
                 <div class="row">
                     <div class="col-lg-12">
                         <textarea name="body" class="tinymce">{!! $term->body !!}</textarea>
