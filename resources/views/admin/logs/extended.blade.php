dcac7b3f991529e09e57a2d53ed5114de3feab2f
sodu <sparsiev@firmidea.com>
2023-02-15 07:38:14 +0500

    extended log view basic things
    issue#197

--- resources/views/admin/logs/extended.blade.php
+++ resources/views/admin/logs/extended.blade.php
@@ -12,12 +12,35 @@
 @endsection
 @section('content')
     <div class="row">
+        <div class="col-lg-12">
+            <div>
 
-        <div class="col-lg-12 mt-2">
-            <div class="card mb-3 fieldset border border-muted">
-                <span class="fieldset-tile text-muted bg-body">Logs</span>
+            </div>
+
+            <div class="card mt-3">
                 <div class="card-body">
-
+                    <table class="table table-striped table-sm small">
+                        <thead>
+                        <tr>
+                            <th>Date</th>
+                            <th>Account</th>
+                            <th>Level</th>
+                            <th>Message</th>
+                            <th>Details</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($logs as $log)
+                            <tr>
+                                <td>{{$log->created_at}}</td>
+                                <td>{{$log->account?->name}}</td>
+                                <td>{{\App\Enums\Core\LogSeverity::from($log->log_level)->getShort()}}</td>
+                                <td>{{$log->log}}</td>
+                                <td>{{$log->detail}}</td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
