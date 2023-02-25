f29ea9ba95015a2165b19681406c99228faf983c
vlchris <chris@vocalogic.com>
2023-02-25 07:23:53 -0500

    removed old styles added new login/forgot and installer css layout

--- resources/views/layouts/installer.blade.php
+++ resources/views/layouts/installer.blade.php
@@ -1,45 +1,116 @@
+
 <!doctype html>
-<html class="no-js " lang="en" data-theme="{{currentMode()}}">
+<html lang="en" data-layout="vertical" data-topbar="light" data-sidebar="dark" data-sidebar-size="lg" data-sidebar-image="none" data-preloader="disable">
 
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <x-seo::meta />
-    <link rel="icon" href="favicon.ico" type="image/x-icon"> <!-- Favicon-->
 
-    <!-- project css file  -->
-    <link rel="stylesheet" href="/assets/css/dataTables.min.css">
-    <link rel="stylesheet" href="/assets/css/select2.min.css">
-    <link rel="stylesheet" href="/assets/css/x-editable.min.css" />
-    <link rel="stylesheet" href="/assets/css/dropify.min.css">
-    <link rel="stylesheet" href="/assets/plugin/rating/rating.css"/>
-    <link rel="stylesheet" href="/assets/js/wm/waitMe.css">
-    <link rel="stylesheet" href="/assets/css/daterangepicker.min.css">
-    <link rel="stylesheet" href="/assets/css/fancybox.min.css">
-    <link rel="stylesheet" href="/assets/css/jquerysteps.min.css">
-    <link rel="stylesheet" href="/assets/css/dataTables.min.css">
-    <link rel="stylesheet" href="/assets/css/fullcalendar.min.css">
-    <link rel="stylesheet" href="/assets/js/jquery.signaturepad.css">
+    <meta charset="utf-8" />
+    <title>{{$title ?? "Login"}}</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="/assets/images/favicon.ico">
 
+    <!-- Layout config Js -->
+    <script src="/assets/js/layout.js"></script>
+    <!-- Bootstrap Css -->
+    <link href="/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <!-- Icons Css -->
+    <link href="/assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+    <!-- App Css-->
+    <link href="/assets/css/app.min.css" rel="stylesheet" type="text/css" />
+    <!-- custom Css-->
+    <link href="/assets/css/custom.min.css" rel="stylesheet" type="text/css" />
 
-    <!-- project css file  -->
-    <link rel="stylesheet" href="/assets/css/luno.style.css">
-    <link rel="stylesheet" href="/assets/css/logic.css">
-    <link rel="stylesheet" href="/assets/css/bootstrapdatepicker.min.css"></head>
+</head>
 
-<body id="layout-1">
+<body>
 
-<!-- start: body area -->
-<div class="wrapper">
+<div class="auth-page-wrapper pt-5">
+    <!-- auth page bg -->
+    <div class="auth-one-bg-position auth-one-bg" id="auth-particles">
+        <div class="bg-overlay"></div>
 
-    <!-- start: page body -->
-    <div class="page-body auth px-xl-4 px-sm-2 px-0 py-lg-2 py-1">
-        <div class="container-fluid">
-            @yield('content')
+        <div class="shape">
+            <svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 1440 120">
+                <path d="M 0,36 C 144,53.6 432,123.2 720,124 C 1008,124.8 1296,56.8 1440,40L1440 140L0 140z"></path>
+            </svg>
         </div>
     </div>
+
+    <!-- auth page content -->
+    <div class="auth-page-content">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="text-center mt-sm-5 mb-4 text-white-50">
+                        <div>
+                            <a href="index.html" class="d-inline-block auth-logo">
+                                @if(currentMode() == 'light' && setting('brandImage.light'))
+                                    <img width="300" src="{{_file(setting('brandImage.light'))?->relative}}" alt="{{setting('brand.name')}}">
+                                @else
+                                    @if(setting('brandImage.dark'))
+                                        <img width="300" src="{{_file(setting('brandImage.dark'))?->relative}}" alt="{{setting('brand.name')}}">
+                                    @endif
+                                @endif
+                            </a>
+                        </div>
+                        <p class="mt-3 fs-15 fw-medium">Welcome to {{setting('brand.name') ?: "Logic"}}</p>
+                    </div>
+                </div>
+            </div>
+            <!-- end row -->
+
+            <div class="row justify-content-center">
+                <div class="col-md-8 col-lg-6 col-xl-5">
+                    @yield('content')
+
+                        <!-- end card body -->
+                    <!-- end card -->
+
+
+
+                </div>
+            </div>
+            <!-- end row -->
+        </div>
+        <!-- end container -->
+    </div>
+    <!-- end auth page content -->
+
+    <!-- footer -->
+    <footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6">
+                    &copy;{{now()->year}} {{setting('brand.name')}}
+                </div>
+                <div class="col-sm-6">
+                    <div class="text-sm-end d-none d-sm-block">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- end Footer -->
 </div>
+<!-- end auth-page-wrapper -->
+
+<!-- JAVASCRIPT -->
+<script src="/assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="/assets/libs/simplebar/simplebar.min.js"></script>
+<script src="/assets/libs/node-waves/waves.min.js"></script>
+<script src="/assets/libs/feather-icons/feather.min.js"></script>
+<script src="/assets/js/pages/plugins/lord-icon-2.1.0.js"></script>
 <script src="/assets/js/plugins.js"></script>
+
+<!-- particles js -->
+<script src="/assets/libs/particles.js/particles.js"></script>
+<!-- particles app js -->
+<script src="/assets/js/pages/particles.app.js"></script>
+<!-- password-addon init -->
+<script src="/assets/js/pages/password-addon.init.js"></script>
 </body>
+
 </html>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,37 +1,7 @@
 @extends('layouts.installer', ['title' => "Login to " . setting('brand.name')])
 
 @section('content')
-    <div class="row">
-        <div class="col-lg-6 d-none d-lg-flex justify-content-center align-items-center">
-            <div style="max-width: 25rem;">
-                <div class="mb-4">
-                    @if(currentMode() == 'light' && setting('brandImage.light'))
-                    <img width="300" src="{{_file(setting('brandImage.light'))?->relative}}" alt="{{setting('brand.name')}}">
-                    @else
-                        @if(setting('brandImage.dark'))
-                        <img width="300" src="{{_file(setting('brandImage.dark'))?->relative}}" alt="{{setting('brand.name')}}">
-                        @endif
-                    @endif
-                </div>
-                <div class="mb-5">
-                    <h4 class="color-900">Login to {{setting('brand.name')}}</h4>
-                </div>
-                <!-- List Checked -->
-                <ul class="list-unstyled mb-5">
-                    <li class="mb-4">
-                        <span class="d-block mb-1 fs-4 fw-light">Get Started</span>
-                        <span class="color-600">Manage your invoices, orders and services. </span>
-                    </li>
-                    <li>
 
-                        <span class="color-600">{{setting('shop.info')}}</span>
-                    </li>
-                </ul>
-
-            </div>
-        </div>
-
-        <div class="col-lg-6 d-flex justify-content-center align-items-center">
 
             <div class="card shadow-sm w-100 p-4 p-md-5" style="max-width: 32rem;">
 
@@ -74,6 +44,6 @@
         </div>
 
 
-    </div>
+
 
 @endsection
